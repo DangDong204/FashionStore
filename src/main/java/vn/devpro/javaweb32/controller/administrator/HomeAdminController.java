e8598dc91f07dcd952300a3e7df69fd96d1bc83a
DangDong204 <165341409+DangDong204@users.noreply.github.com>
2025-10-18 23:20:00 +0700

    Update Chart

--- src/main/java/vn/devpro/javaweb32/controller/administrator/HomeAdminController.java
+++ src/main/java/vn/devpro/javaweb32/controller/administrator/HomeAdminController.java
@@ -1,16 +1,37 @@
 package vn.devpro.javaweb32.controller.administrator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.devpro.javaweb32.controller.BaseController;
+import vn.devpro.javaweb32.service.SaleOrderProductService;
 
 @Controller
 public class HomeAdminController extends BaseController{
 	
+	@Autowired SaleOrderProductService sops;
+	
 	@RequestMapping(value ="/admin/home")
 	public String list(Model model) {
+		
+		List<Object[]> topProducts = sops.getTop5SellingProducts();
+
+        List<String> productNames = new ArrayList<>();
+        List<Integer> productSales = new ArrayList<>();
+
+        for (Object[] row : topProducts) {
+            productNames.add((String) row[0]);
+            productSales.add(((Number) row[1]).intValue());
+        }
+
+        model.addAttribute("productNames", productNames);
+        model.addAttribute("productSales", productSales);
+        
 		return "administrator/home";
 	}
 }
--- src/main/java/vn/devpro/javaweb32/service/SaleOrderProductService.java
+++ src/main/java/vn/devpro/javaweb32/service/SaleOrderProductService.java
@@ -1,5 +1,9 @@
 package vn.devpro.javaweb32.service;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Service;
 
 import vn.devpro.javaweb32.model.SaleOrderProduct;
@@ -12,5 +16,18 @@ public class SaleOrderProductService extends BaseService<SaleOrderProduct>{
 		
 		return SaleOrderProduct.class;
 	}
+	
+	// Lấy top 5 sản phẩm bán chạy nhất
+    @SuppressWarnings("unchecked")
+    public List<Object[]> getTop5SellingProducts() {
+        String sql = "SELECT p.name AS product_name, SUM(sop.quantity) AS total_sold "
+                   + "FROM tbl_sale_order_product sop "
+                   + "JOIN tbl_product p ON sop.product_id = p.id "
+                   + "GROUP BY p.name "
+                   + "ORDER BY total_sold DESC "
+                   + "LIMIT 5";
 
+        Query query = entityManager.createNativeQuery(sql);
+        return query.getResultList();
+    }
 }
